3e22774a2e8d284253a0f049e1bc3430a79234e6
cshadow mac <cshadow@cshadow.com>
2016-11-10 14:57:13 +0900

    my layers

--- Keyboards/ergodox.bash
+++ Keyboards/ergodox.bash
@@ -23,7 +23,8 @@ BaseMap="defaultMap leftHand slave1 rightHand"
 # This is the default layer of the keyboard
 # NOTE: To combine kll files into a single layout, separate them by spaces
 # e.g.  DefaultMap="mylayout mylayoutmod"
-DefaultMap="mdergo1Overlay lcdFuncMap"
+# DefaultMap="mdergo1Overlay lcdFuncMap"
+DefaultMap="MDErgo1-Default-0 lcdFuncMap"
 
 # This is where you set the additional layers
 # NOTE: Indexing starts at 1
@@ -31,8 +32,11 @@ DefaultMap="mdergo1Overlay lcdFuncMap"
 # e.g.  PartialMaps[1]="layer1 layer1mod"
 #       PartialMaps[2]="layer2"
 #       PartialMaps[3]="layer3"
-PartialMaps[1]="iced_func"
-PartialMaps[2]="iced_numpad"
+# PartialMaps[1]="iced_func"
+# PartialMaps[2]="iced_numpad"
+PartialMaps[1]="MDErgo1-Default-1 lcdFuncMap"
+PartialMaps[2]="MDErgo1-Default-2 lcdFuncMap"
+PartialMaps[3]="MDErgo1-Default-3 lcdFuncMap"
 
 
 
